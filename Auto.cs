6890f9a6af8ee7ba9ccbd311924f60c174f82b51
1eG0ist <diadypetya@yandex.ru>
2023-10-02 22:28:15 +0300

    Добавьте файлы проекта.

---
+++ Auto.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Auto
+    {
+        private static int number_of_auto = 1;
+        private static List<int[]> autos_positions = new List<int[]>();
+        private int n_of_auto;
+        private string color;
+        private string name;
+        private float fuel_tank_size;
+        private float coms_per_100_km;
+        private float coms_per_km_multiplier;
+        private float fuel_now;
+        private float mileage;
+        private int x;
+        private int y;
+        private float speed;
+        private float health;
+
+
+        public Auto(string name, string color, float fuel_tank_size, float coms_per_100_km, float fuel_now, float mileage, int x, int y)
+        {
+            this.color = color;
+            this.name = name;
+            this.fuel_tank_size = fuel_tank_size;
+            this.coms_per_100_km = coms_per_100_km;
+            this.fuel_now = fuel_now;
+            this.mileage = mileage;
+            this.x = x;
+            this.y = y;
+            int[] _coords = new int[] { x, y };
+            autos_positions.Add(_coords);
+            health = 100;
+            speed = 0;
+            coms_per_km_multiplier = 1;
+            n_of_auto=number_of_auto-1;
+            number_of_auto++;
+        }
+
+        public void goTo(int x, int y)
+        {
+            if (speed == 0)
+            {
+                Console.WriteLine("You have 0 speed, pls up your speed to travel, press any key to continue");
+                Console.ReadKey();
+                return;
+            }
+            float distance = calculateDistance(x, y);
+            float needs_fuel = distance * coms_per_100_km * coms_per_km_multiplier / 100;
+            if (needs_fuel > fuel_now)
+            {
+                Console.WriteLine("You don't have enough fuel, you want to refuel? (1 - Yes, 2 -No)");
+                string answer = Console.ReadLine();
+                if (answer == "1") {
+                    refuel();
+                    goTo(x, y);
+                } else
+                {
+                    Console.WriteLine("You haven't gone anywhere");
+                }
+            } else
+            {
+                for (int i = 0; i < autos_positions.Count; i++)
+                {
+                    if (autos_positions[i][0] == x && autos_positions[i][1] == y)
+                    {
+                        autoCrash();
+                        break;
+                    }
+                }
+                setPosition(x, y);
+                autos_positions[n_of_auto] = new int[] { x, y };
+                this.mileage += distance;
+                fuel_now -= needs_fuel;
+            }
+        } 
+
+        public float calculateDistance(int x, int y)
+        {
+            return (float)Math.Sqrt(Math.Pow(this.x - x, 2) + Math.Pow(this.y - y, 2));
+        }
+
+        public void speedUp(float up_on)
+        {
+            if (speed + up_on >= 500)
+            {
+                Console.WriteLine("Too big speed");
+            } else
+            {
+                speed += up_on;
+                updateComsPerKmMultiplier();
+            }
+        }
+
+        public void autoCrash()
+        {
+            health -= 20;
+            Console.WriteLine("The auto crashed into another auto!!! Press any key to continue: ");
+            Console.ReadKey();
+        }
+
+        public void speedDown(float down_on)
+        {
+            if (speed - down_on < 0)
+            {
+                Console.WriteLine("Error: speed is under of 0, seted speed = 0");
+                speed = 0;
+            } else
+            {
+                speed -= down_on;
+                updateComsPerKmMultiplier();
+            }
+        }
+
+        public void updateComsPerKmMultiplier()
+        {
+            if (speed < 50) coms_per_km_multiplier = 1;
+            else if (speed >= 50 && speed < 100) coms_per_km_multiplier = 1.1f;
+            else if (speed >= 100 && speed < 150) coms_per_km_multiplier = 1.2f;
+            else if (speed >= 150 && speed < 250) coms_per_km_multiplier = 1.3f;
+            else if (speed >= 250 && speed < 350) coms_per_km_multiplier = 1.4f;
+            else if (speed >= 350) coms_per_km_multiplier = 1.5f;
+        }
+
+        public void setPosition(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void changeColor(string new_color)
+        {
+            color = new_color;
+        }
+
+        public void setFuelNow(float fuel_now)
+        {
+            this.fuel_now = fuel_now;
+        }
+
+        public void refuel()
+        {
+            fuel_now = fuel_tank_size;
+        }
+
+        public void setNewColor(string new_color)
+        {
+            color = new_color;
+        }
+
+        public void modernFuelTank(float new_fuel_tank_size)
+        {
+            fuel_tank_size = new_fuel_tank_size;
+        }
+
+        public float getSpeed()
+        {
+            return speed;
+        }
+
+        public float getHealth()
+        {
+            return health;
+        }
+
+        public override string ToString()
+        {
+            return $"Number of created auto - {n_of_auto}\nName - {name}\nColor - {color}\nFuel tank size - {fuel_tank_size}\n" +
+                $"Coms per 100 km - {coms_per_100_km}\nFuel now - {fuel_now}\nMileage - {mileage}\nX - {x}\nY - {y}\nSpeed - {speed}\nHealth - {health}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,130 @@
+﻿using System.Transactions;
+
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello");
+            List<Auto> vsevolods_autopark = new List<Auto>()
+            {
+                new Auto("BMW", "DARK", 100, 30, 86, 0, 0, 0),
+                new Auto("AUDI", "BLUE", 120, 45, 64, 0, 1, 0),
+                new Auto("LADA", "NO", 50, 19, 25, 0, 2, 0),
+            };
+            for (int i = 0; i < vsevolods_autopark.Count; i++)
+            {
+                Console.WriteLine($"Enter fuel tank size of auto №{i+1}");
+                float fuel_size = float.Parse(Console.ReadLine());
+                vsevolods_autopark[i].modernFuelTank(fuel_size);
+                Console.WriteLine($"Enter how many fuel auto №{i + 1} have now (0-{fuel_size}): ");
+                vsevolods_autopark[i].setFuelNow(float.Parse(Console.ReadLine()));
+            }
+            int selected_auto = 0;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine($"Q - Show auto info\nW - Change color\nE - Modern fuel tank\nR - Refuel\nT - Go\nY - Change selected auto (1-3)\nU - Up speed\nI - Down speed");
+                    ConsoleKey input_key = Console.ReadKey().Key;
+                    Console.WriteLine();
+                    int counter = 0;
+                    for (int i = 0; i < vsevolods_autopark.Count; i++)
+                    {
+                        if (vsevolods_autopark[i].getHealth() <= 0)
+                        {
+                            counter++;
+                        }
+                    }
+                    if (counter == 3)
+                    {
+                        Console.WriteLine("All autos was total crashed! You Lose! Bye!");
+                        return;
+                    }
+
+                    if (vsevolods_autopark[selected_auto].getHealth() <= 0)
+                    {
+                        try
+                        {
+                            Console.WriteLine($"Auto № {selected_auto + 1} full crashed, enter new number of auto (at start you had 3): ");
+                            selected_auto = int.Parse(Console.ReadLine()) - 1;
+                            continue;
+                        }
+                        catch
+                        {
+                            continue;
+                        }
+                    }
+
+                    switch (input_key)
+                    {
+                        case ConsoleKey.Q:
+                            Console.WriteLine(vsevolods_autopark[selected_auto]);
+                            next();
+                            Console.Clear();
+                            break;
+
+                        case ConsoleKey.W:
+                            Console.WriteLine("Enter new color: ");
+                            vsevolods_autopark[selected_auto].setNewColor(Console.ReadLine());
+                            Console.Clear();
+                            break;
+
+                        case ConsoleKey.E:
+                            Console.WriteLine("Enter new fuel tank size: ");
+                            vsevolods_autopark[selected_auto].modernFuelTank(float.Parse(Console.ReadLine()));
+                            Console.Clear();
+                            break;
+
+                        case ConsoleKey.R:
+                            vsevolods_autopark[selected_auto].refuel();
+                            next();
+                            Console.Clear();
+                            break;
+
+                        case ConsoleKey.T:
+                            Console.WriteLine("Enter integer x, y coords: ");
+                            string coords = Console.ReadLine();
+                            vsevolods_autopark[selected_auto].goTo(int.Parse(coords.Split(' ')[0]), int.Parse(coords.Split(' ')[1]));
+                            Console.Clear();
+                            break;
+
+                        case ConsoleKey.Y:
+                            Console.WriteLine("Enter new selected auto: ");
+                            selected_auto = int.Parse(Console.ReadLine()) - 1;
+                            Console.Clear();
+                            break;
+
+                        case ConsoleKey.U:
+                            Console.WriteLine($"Enter on how many you want to up speed (now is {vsevolods_autopark[selected_auto].getSpeed()})");
+                            vsevolods_autopark[selected_auto].speedUp(float.Parse(Console.ReadLine()));
+                            Console.Clear();
+                            break;
+
+                        case ConsoleKey.I:
+                            Console.WriteLine($"Enter on how many you want to down speed (now is {vsevolods_autopark[selected_auto].getSpeed()})");
+                            vsevolods_autopark[selected_auto].speedDown(float.Parse(Console.ReadLine()));
+                            Console.Clear();
+                            break;
+
+                        default:
+                            Console.Clear();
+                            break;
+                    }
+                    Console.WriteLine("Введите номер ");
+                } catch (Exception ex)
+                {
+                    continue;
+                }
+            }
+        }
+
+        static void next()
+        {
+            Console.WriteLine("\nPress any keys to go to menu: ");
+            Console.ReadKey();
+        }
+    }
+}
